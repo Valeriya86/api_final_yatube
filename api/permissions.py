71c41c8abffc46640c377b53fd4892dd974b62fc
Valeriya <valeriya_86_@mail.ru>
2021-05-25 18:20:51 +0300

    eigth commit

--- api/permissions.py
+++ api/permissions.py
@@ -5,5 +5,4 @@ class IsAuthorOrReadOnlyPermission(BasePermission):
 
     def has_object_permission(self, request, view, obj):
 
-        if request.method in SAFE_METHODS or obj.author == request.user:
-            return obj.author
+        return request.method in SAFE_METHODS or obj.author == request.user
